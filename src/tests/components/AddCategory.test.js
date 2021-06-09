05a65c81d79d5ace0e7e51ec072ab7cd4660e89c
Alan1807 <alannmv@hotmail.com>
2021-06-09 11:30:04 -0800

    Finish AddCategory's test

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -6,12 +6,30 @@ import { AddCategory } from "../../components/AddCategory";
 describe('Pruebas en <AddCategory />', () => {
     
     const setCategories = () => {};
+    const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );    
 
-    test('debe de mostrarse correctamente', () => {
-
-        const wrapper = shallow( <AddCategory setCategories={ setCategories } /> );
+    test('debe de mostrarse correctamente', () => {        
+        
         expect( wrapper ).toMatchSnapshot();
 
     });
+
+    test('debe de cambiar la caja de texto', () => {
+        
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', { target: { value } });
+
+        // input.simulate('change', {
+        //     target: {
+        //         value: value
+        //     }
+        // });
+
+        expect( wrapper.find('p').text().trim() ).toBe( value );
+
+    });
+    
 
 });
